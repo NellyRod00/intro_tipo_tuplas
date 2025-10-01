d88f48178dc8715c6054b29c87cfd83471340db5
NellyRod00 <nelly.rod90@gmail.com>
2025-10-01 17:06:15 -0600

    Subiendo los archivos

---
+++ Downloads/intro_tipo_tuplas/Tuplas_for.py
@@ -0,0 +1,16 @@
+# Definimos una tupla con nombres
+nombres = ("Ana", "Luis", "María", "Carlos")
+print(f"la tupla de nombres es:\n{nombres}")    # Imprieme la tupla completa
+
+    # Recorriendo la tupla con un for
+print("Lista de nombres:")
+for nombre in nombres:
+    print(nombre)
+
+    # Ejemplo con tupla de números
+numeros = (10, 20, 30, 40, 50)
+print(f"los números en la tupla son:\n{numeros}")  # Imprime la tupla completa
+
+print("Números multiplicados por 2:")   # Recorrie la tupla y multiplicando cada número por 2
+for n in numeros:
+    print(n * 2)
---
+++ Downloads/intro_tipo_tuplas/Tuplas_while.py
@@ -0,0 +1,11 @@
+# Definimos una lista con frutas
+frutas = ("manzana", "banana", "cereza", "uva")
+print(f"La tupla de frutas es:\n{frutas}")  # Imprime la tupla completa
+
+# Usamos while para recorrer
+inicia = 0 
+print("Recorriendo la tupla con while:") 
+
+while inicia < len(frutas):
+    print(f"En la posición {inicia}: {frutas[inicia]}") # Imprime cada fruta con su posición
+    inicia += 1 # Incrementa el índice
